b7384565d462e638125644238513b22a08e87d90
Rukki0109 <lukiya0109@icloud.com>
2025-03-30 17:16:58 +0900

    コメント機能

--- routes/comments.js
+++ routes/comments.js
@@ -13,26 +13,18 @@ router.post("/", async (req, res) => {
 });
 
 // 特定の投稿のコメント一覧取得
-// router.get("/:postId", async (req, res) => {
-//   try {
-//     const comments = await Comment.find({ postId: req.params.postId })
-//       .sort({ createdAt: -1 })
-//       .populate("userId", "username profilePicture"); // ← 追加
-//     res.status(200).json(comments);
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
-
-
 router.get("/:postId", async (req, res) => {
   try {
-    const comments = await Comment.find({ postId: req.params.postId }).sort({ createdAt: -1 });
+    const comments = await Comment.find({ postId: req.params.postId })
+      .sort({ createdAt: -1 })
+      .populate("userId", "username profilePicture"); // ← 追加
     res.status(200).json(comments);
   } catch (err) {
     res.status(500).json(err);
   }
 });
+
+
 
 
 module.exports = router;
